4899c7f572cc9bfb248ead849af58ae4d4abfd16
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-12-15 15:34:15 +0900

    Design: 폰트 설정

--- src/style/global.ts
+++ src/style/global.ts
@@ -2,12 +2,17 @@ import "sanitize.css";
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    body {
+    html {
+        font-size: 10px;
+        font-family: Roboto, Arial, sans-serif;
+
+        body {
         padding: 0;
         margin: 0;
         background-color: ${({ theme }) => (theme.name === "light" ? "white" : "black")};
+    }  
     }
-
+    
     * {
         color: ${({ theme }) => (theme.name === "light" ? "black" : "white")};
     }
