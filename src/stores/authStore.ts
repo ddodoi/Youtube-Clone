254987da785975d1929f46b8f883c12fbed79946
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-12-16 10:40:59 +0900

    Fix: 유저 정보가 유지되도록 수정

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,35 +1,40 @@
 import { create } from "zustand";
 import { LoginResponse } from "../mock/auth.mock";
 
-export const getToken = () => {
-    return localStorage.getItem("token");
-};
-
-const setToken = (token: string) => {
-    localStorage.setItem("token", token);
-};
-
-export const removeToken = () => {
-    localStorage.removeItem("token");
-};
-
 type StoreState = LoginResponse & {
     isLoggedIn: boolean;
     storeLogin: (data: LoginResponse) => void;
     storeLogout: () => void;
 };
 
+export const getToken = () => {
+    return localStorage.getItem("token");
+};
+
+const setLocalStorage = ({ token, userName, avatarURL }: LoginResponse) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("avatarURL", avatarURL);
+};
+
+export const removeLocalStorage = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userName");
+    localStorage.removeItem("avatarURL");
+};
+
 export const useAuthStore = create<StoreState>((set) => ({
     isLoggedIn: getToken() ? true : false,
-    avatarURL: "",
-    userName: "",
-    token: "",
-    storeLogin: ({ token, avatarURL, userName }) => {
-        setToken(token);
+    avatarURL: localStorage.getItem("avatarURL") || "",
+    userName: localStorage.getItem("userName") || "",
+    token: getToken() || "",
+    storeLogin: (userData) => {
+        setLocalStorage(userData);
+        const { avatarURL, userName, token } = userData;
         set(() => ({ isLoggedIn: true, avatarURL, userName, token }));
     },
     storeLogout: () => {
-        removeToken();
+        removeLocalStorage();
         set(() => ({ isLoggedIn: false, avatarURL: "", userName: "", token: "" }));
     },
 }));
