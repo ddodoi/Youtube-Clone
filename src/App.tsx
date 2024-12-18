a9b535f72be05fdc8f0bb00c000dd096828bb4f9
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-12-18 11:30:33 +0900

    Revert "반응형 비디오 그리드 호버 효과 구현"

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./components/layout/Layout";
 import Error from "./components/common/Error";
 import { ThemeProvider } from "styled-components";
 import { useThemeStore } from "./stores/themeStore";
 import { GlobalStyle } from "./style/global";
 import Login from "./pages/Login";
-import MainPage from "./components/mainPage/MainPage";
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 60 * 1000, // 1분
-            retry: 1,
-        },
-    },
-});
+import MainPage from "./pages/MainPage";
 
 const router = createBrowserRouter([
     {
@@ -38,12 +28,10 @@ function App() {
     const { themeName, getTheme } = useThemeStore();
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={getTheme(themeName)}>
-                <GlobalStyle />
-                <RouterProvider router={router} />
-            </ThemeProvider>
-        </QueryClientProvider>
+        <ThemeProvider theme={getTheme(themeName)}>
+            <GlobalStyle />
+            <RouterProvider router={router} />
+        </ThemeProvider>
     );
 }
 
