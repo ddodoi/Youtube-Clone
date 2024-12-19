88eb04e9be4cf1e11ad36e28d5a2de3dc3175deb
Youjin Kim <gopar0101@gmail.com>
2024-12-19 13:35:14 +0900

    Hot Fix 최종

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./components/layout/Layout";
 import Error from "./components/common/Error";
 import { ThemeProvider } from "styled-components";
 import { useThemeStore } from "./stores/themeStore";
 import { GlobalStyle } from "./style/global";
 import Login from "./pages/Login";
-import MainPage from "./pages/MainPage";
-import SearchResult from "./pages/SearchResult";
+import MainPage from "./components/mainPage/MainPage";
+
+// QueryClient 인스턴스를 컴포넌트 외부에서 생성
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000,
+            retry: 1,
+        },
+    },
+});
 
 const router = createBrowserRouter([
     {
@@ -23,25 +33,19 @@ const router = createBrowserRouter([
         element: <Login />,
         errorElement: <Error />,
     },
-    {
-        path: "/results",
-        element: (
-            <Layout>
-                <SearchResult />
-            </Layout>
-        ),
-        errorElement: <Error />,
-    },
 ]);
 
 function App() {
     const { getTheme } = useThemeStore();
-
+    
     return (
-        <ThemeProvider theme={getTheme()}>
-            <GlobalStyle />
-            <RouterProvider router={router} />
-        </ThemeProvider>
+        // QueryClientProvider를 최상위에 배치
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={getTheme()}>
+                <GlobalStyle />
+                <RouterProvider router={router} />
+            </ThemeProvider>
+        </QueryClientProvider>
     );
 }
 
