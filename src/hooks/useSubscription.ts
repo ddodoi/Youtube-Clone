24caeb4b52225b79a12c81560a4aff5f711661a8
Youjin Kim <gopar0101@gmail.com>
2024-12-21 03:56:26 +0900

    비디오, 비디오Info, 비디오컨트롤러 컴포넌트 1차 구현

--- src/hooks/useSubscription.ts
+++ src/hooks/useSubscription.ts
@@ -1,17 +1,9 @@
-import { useEffect, useState } from "react";
 import { fetchSubInfo } from "@apis/user.api";
-import { SubscriptionResponse } from "@@types/user.type";
+import { useQuery } from "@tanstack/react-query";
 
 export const useSubscription = () => {
-    const [subscriptions, setSubscriptions] = useState<SubscriptionResponse[]>([]);
-    const [moreSubscriptions, setMoreSubscriptions] = useState<SubscriptionResponse[]>([]);
-
-    useEffect(() => {
-        fetchSubInfo().then((res: SubscriptionResponse[]) => {
-            setSubscriptions(res.map((sub) => ({ ...sub })).slice(0, 7));
-            setMoreSubscriptions(res.map((sub) => ({ ...sub })).slice(7));
-        });
-    }, []);
-
-    return { subscriptions, moreSubscriptions };
+    return useQuery({
+        queryKey: ["subscriptions"],
+        queryFn: fetchSubInfo,
+    });
 };
