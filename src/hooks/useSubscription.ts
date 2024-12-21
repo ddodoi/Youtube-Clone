78f5a49c0bf38cebb60b256e1e059aa9b3262795
Youjin Kim <gopar0101@gmail.com>
2024-12-21 16:16:55 +0900

    useSubscription.ts -> cancel changes

--- src/hooks/useSubscription.ts
+++ src/hooks/useSubscription.ts
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import { fetchSubInfo } from "@apis/user.api";
-import { useQuery } from "@tanstack/react-query";
+import { SubscriptionResponse } from "@@types/user.type";
 
 export const useSubscription = () => {
-    return useQuery({
-        queryKey: ["subscriptions"],
-        queryFn: fetchSubInfo,
-    });
+    const [subscriptions, setSubscriptions] = useState<SubscriptionResponse[]>([]);
+    const [moreSubscriptions, setMoreSubscriptions] = useState<SubscriptionResponse[]>([]);
+
+    useEffect(() => {
+        fetchSubInfo().then((res: SubscriptionResponse[]) => {
+            setSubscriptions(res.map((sub) => ({ ...sub })).slice(0, 7));
+            setMoreSubscriptions(res.map((sub) => ({ ...sub })).slice(7));
+        });
+    }, []);
+
+    return { subscriptions, moreSubscriptions };
 };
