bbcd3330a5c7f21a1ef5e2d4ba149dd0b9cc7100
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-12-21 21:36:33 +0900

    Feat: useInterserctionObserver 훅 구현

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+
+export const useIntersectionObserver = (callback: IntersectionObserverCallback) => {
+    const targetRef = useRef<Element>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(callback);
+
+        if (targetRef.current) {
+            observer.observe(targetRef.current);
+        }
+        return () => {
+            if (targetRef.current) {
+                observer.unobserve(targetRef.current);
+            }
+        };
+    });
+
+    return { targetRef };
+};
