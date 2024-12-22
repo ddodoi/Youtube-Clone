07f89081072ad7aaead44c0c7c1a14abfe078a23
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-12-22 14:02:47 +0900

    Refactor: SVG 컴포넌트 사이즈 조정 문제 해결

--- src/components/common/SVG.tsx
+++ src/components/common/SVG.tsx
@@ -6,13 +6,22 @@ interface Props {
 }
 
 const SVG: React.FC<Props> = ({ size, children }) => {
-    return <SVGStyle style={{ width: size, height: size }}>{children}</SVGStyle>;
+    return (
+        <SVGStyle style={{ width: size, height: size }} size={size}>
+            {children}
+        </SVGStyle>
+    );
 };
 
-const SVGStyle = styled.div`
+const SVGStyle = styled.div<{ size?: number }>`
     display: flex;
     align-items: center;
     justify-content: center;
+
+    svg {
+        width: ${({ size }) => size}px;
+        height: ${({ size }) => size}px;
+    }
 `;
 
 export default SVG;
