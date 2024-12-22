98e4dbc84a2f9bf97769a7657756dcb31db48b09
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-12-22 14:31:34 +0900

    Refactor: useInterserctionObserver 훅 옵션 매개변수 추가

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,10 +1,19 @@
 import { useEffect, useRef } from "react";
 
-export const useIntersectionObserver = (callback: IntersectionObserverCallback) => {
+interface ObserverOption {
+    root?: Element | null;
+    rootMargin?: string;
+    threshold?: number | number[];
+}
+
+export const useIntersectionObserver = (
+    callback: IntersectionObserverCallback,
+    options?: ObserverOption,
+) => {
     const targetRef = useRef(null);
 
     useEffect(() => {
-        const observer = new IntersectionObserver(callback);
+        const observer = new IntersectionObserver(callback, options);
 
         if (targetRef.current) {
             observer.observe(targetRef.current);
